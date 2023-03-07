de7956f9b3cbb8742e428a91d1f6856b535f96bd
buer <1585480284@qq.com>
2023-03-07 09:45:06 +0800

    feat: 完善测试前置处理； 新增 reverse、top 指令处理； 新增 移除函数及其测试；

--- src/examples/todoList/hooks/useTodoItem.js
+++ src/examples/todoList/hooks/useTodoItem.js
@@ -1,18 +1,42 @@
 import { defineStore } from "pinia";
+import {ref} from 'vue'
 
 export const TodoStore = defineStore('todo', () => {
-    const todos = []
+    const todos = ref([])
+    let id = 1
+
+    const REVERSECOMMAND = 'reverse'
+    const TOPCOMMAND = 'top'
 
     function addTodoItem(title) {
-        const todo = {
-            title,
+
+        let isTop = false
+        if (title.startsWith(`${REVERSECOMMAND}:`)) {
+            title = title.slice(REVERSECOMMAND.length + 1).split('').reverse().join('')
         }
 
-        todos.push(todo)
+        if (title.startsWith(`${TOPCOMMAND}:`)) {
+            isTop = true
+            title = title.slice(TOPCOMMAND.length + 1)
+        }
+
+        const todo = {
+            title,
+            id: id++,
+        }
+
+        isTop ? todos.value.unshift(todo) : todos.value.push(todo)
+
+        return todo
+    }
+
+    function removeTodoItem(todoId) {
+        todos.value = todos.value.filter(todo => todo.id != todoId)
     }
 
     return {
         todos,
         addTodoItem,
+        removeTodoItem,
     }
 })
--- tests/todoList/addTodoItem.spec.js
+++ tests/todoList/addTodoItem.spec.js
@@ -1,10 +1,47 @@
-import { createPinia } from "pinia";
-import { describe, it, expect } from "vitest";
+import { createPinia, setActivePinia } from "pinia";
+import { describe, it, expect, beforeEach } from "vitest";
 import { TodoStore } from '../../src/examples/todoList/hooks/useTodoItem'
 
 describe('todoList test', () => {
-    it('add todo item', ()=>{
-        const todo = TodoStore.addTodoItem('ok')
-        expect(TodoStore.todos[0].title).toBe('ok')
+    beforeEach(() => {
+        setActivePinia(createPinia())
     })
+
+    it('test add todo item', () => {
+        const store = TodoStore()
+        const todo = store.addTodoItem('ok')
+        expect(store.todos[0]).toEqual(todo)
+    })
+
+    it('test reverse command', () => {
+        const store = TodoStore()
+        store.addTodoItem('reverse:ok')
+        expect(store.todos[0].title).toBe('ko')
+
+        // TODO 边界处理
+        // store.addTodoItem('reverse :ok')
+        // expect(store.todos[1].title).toBe('ko')
+    })
+
+    it('test top command', () => {
+        const store = TodoStore()
+        store.addTodoItem('ok')
+        const todo = store.addTodoItem('top:love')
+       
+        expect(store.todos[0].title).toBe('love')
+
+        // store.addTodoItem('top :love')
+        // expect(store.todos[0].title).toBe('love')
+    })
+
+    it('test remove todo item', () => {
+        const store = TodoStore()
+        store.addTodoItem('ok')
+        const todo = store.addTodoItem('top:love')
+
+        store.removeTodoItem(todo.id)
+       
+        expect(store.todos.length).toBe(1)
+    })
+
 })
